bc038354b2eb8aa2e9d414909245c3229c3b050f
Gordon <gordonmes@gmail.com>
2021-03-16 15:42:47 +0100

    copy to clipboard logic, basic design for confirmation message

--- components/LastAddedUrl.component.tsx
+++ components/LastAddedUrl.component.tsx
@@ -1,16 +1,40 @@
-const LastAddedUrl = ({ shortUrl }) => {
+import { useState } from "react";
+
+const LastAddedUrl = ({ shortUrl: key }) => {
   // todo : copy button logic
-  const completeUrl = `shurl.gordonmes.dev/${shortUrl}`;
+  const completeUrl = `shurl.gordonmes.dev/${key}`;
+  const [copyMessage, setCopyMessage] = useState("");
+
+  const copyToClipBoard = async (toCopy) => {
+    try {
+      /* 
+      https://web.dev/async-clipboard/ 
+      */
+      await navigator.clipboard.writeText(toCopy);
+      setCopyMessage("Copied !");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div
       className={`${
-        shortUrl === "" ? "hidden" : "block"
+        key === "" ? "hidden" : "block"
       } border-gray-200 rounded-lg border mt-7 sm:mt-14 flex items-center sm:justify-center lg:justify-start`}
     >
-      <div className="w-full flex hover:text-orange-400 items-center justify-left px-3 py-3 text-gray-200 font-medium text-sm md:py-4 md:text-lg md:px-10">
-        <a href={`https://${completeUrl}`}>{completeUrl}</a>
+      <div className="w-full flex items-center justify-left px-3 py-3 text-gray-200 font-medium text-sm md:py-4 md:text-lg md:px-10">
+        <a href={`https://${completeUrl}`} className="hover:text-orange-400">
+          {completeUrl}
+        </a>
       </div>
-      <div className="mr-5 font-medium hover:text-orange-200">Copy</div>
+      <button
+        onClick={() => copyToClipBoard(completeUrl)}
+        className="mr-5 font-medium hover:text-orange-200"
+      >
+        Copy
+      </button>
+      {copyMessage}
     </div>
   );
 };
